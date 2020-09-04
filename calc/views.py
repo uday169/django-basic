6c76c3db8d05dca6f76ee60e6baae2497e4794b8
Uday Pratap <udaypratap169@gmail.com>
2020-09-04 15:57:19 +0530

    dynamic name

--- calc/views.py
+++ calc/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-   return render(request, 'home.html')
+   return render(request, 'home.html', {'name': 'Singh'})
