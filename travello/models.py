97300cde35ecaa9173cf1c0bbce59e55afffc0c7
Uday Pratap <udaypratap169@gmail.com>
2020-09-05 02:07:20 +0530

    setup postgressql and databse migrated

--- travello/models.py
+++ travello/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class Destination:
-   id: int
-   name: str
-   img: str
-   desc: str
-   price: int
+class Destination(models.Model):
+   name = models.CharField(max_length= 100)
+   img = models.ImageField(upload_to='pics/')
+   desc = models.TextField()
+   price = models.IntegerField()
+   offer = models.BooleanField(default=False)
