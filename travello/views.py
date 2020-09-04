1927b8cf99e00325445c0d91ebfd601d2b1697df
Uday Pratap <udaypratap169@gmail.com>
2020-09-05 02:48:59 +0530

    admin site registered and data fetching

--- travello/views.py
+++ travello/views.py
@@ -3,27 +3,6 @@ from .models import Destination
 
 def index(request):
    
-   dest1 = Destination()
-   dest1.name = "Mumbai"
-   dest1.desc = "City That never sleep"
-   dest1.img = "destination_1.jpg"
-   dest1.price = 700
-   dest1.offer = False
-   
-   dest2 = Destination()
-   dest2.name = "Hydrabed"
-   dest2.desc = "Hydrabedi Biryani"
-   dest2.img = "destination_2.jpg"
-   dest2.price = 650
-   dest2.offer = True
-   
-   dest3 = Destination()
-   dest3.name = "Banglore"
-   dest3.desc = "Silcon City"
-   dest3.img = "destination_3.jpg"
-   dest3.price = 750
-   dest3.offer = False
-   
-   dests = [dest1, dest2, dest3]
+   dests =  Destination.objects.all()
    
    return render(request, 'index.html' , {'dests': dests})
